b6919f3c2e398bd82bc07b1e4bb979dd153346ef
GilTads <gil.tads@gamil.com>
2022-12-20 20:06:59 -0400

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,80 @@
+import { Alert, FlatList, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import { Participant } from '../../components/Participant';
+import { styles } from './styles';
+
+export function Home () {
+  const participants = ['Ali Alhjazihi'];
+
+  function handleParticipantAdd (participant:string) {
+
+    if (participants.includes("Gil Santos")) {
+      return Alert.alert("Participante já existe", "Já existe um participante na lista com esse nome" )
+    }
+
+    participants.push('Ana');
+
+    console.log(participants);
+    
+  }
+
+  function handleParticipantRemove (participant: string) {
+
+    Alert.alert("Remover",`Deseja remover o participante ${participant}?`, [
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert('Participante excluído!')
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
+
+    console.log(`Você clicou em remover o participante ${participant}`);
+    
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>SWU</Text>
+      <Text style={styles.eventDate}>Sábado, 10 de dezembro de 2022</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} 
+          onPress={() => handleParticipantAdd("Adicionou")}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList 
+        data={participants}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Participant 
+            key={item} 
+            name={item} 
+            onRemove={() => handleParticipantRemove(item)} 
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participaentes à sua lista de presença
+          </Text>
+        )}
+      />
+
+      
+    </View>
+  );
+}
